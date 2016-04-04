83069b41333d4776cd16505259a3cc11d6ca52b2
Matthew Rigdon <matt@bitium.com>
2016-04-04 15:36:12 -0700

    progress on Account tab

---
+++ SomaSole/TrainerRequestConfirmViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TrainerRequestConfirmViewController.swift
+//  SomaSole
+//
+//  Created by Matthew Rigdon on 4/4/16.
+//  Copyright © 2016 SomaSole. All rights reserved.
+//
+
+import UIKit
+
+class TrainerRequestConfirmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func tappedNevermind(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
