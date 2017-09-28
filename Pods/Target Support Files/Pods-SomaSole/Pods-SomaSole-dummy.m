713d19b9f81951df0874601dc2895dfe125245bb
Matthew Rigdon <rigdonmr@gmail.com>
2017-09-28 07:53:28 -0700

    removed cocoapods

--- Pods/Target Support Files/Pods-SomaSole/Pods-SomaSole-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SomaSole : NSObject
-@end
-@implementation PodsDummy_Pods_SomaSole
-@end
--- Pods/Target Support Files/Pods-SomaSoleUITests/Pods-SomaSoleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SomaSoleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SomaSoleUITests
-@end
